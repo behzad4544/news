0677f9fd660994c06e530a85412368a288023685
behzad <behzad.kermanii@gmail.com>
2023-05-07 15:40:55 +0330

    delete & update category done

--- activities/Admin/Category.php
+++ activities/Admin/Category.php
@@ -23,4 +23,22 @@ class Category extends Admin
         $db->insert('categories', array_keys($request), $request);
         $this->redirect("admin/category");
     }
+    public function edit($id)
+    {
+        $db = new Database();
+        $category = $db->select('SELECT * FROM categories WHERE `id` =?', [$id])->fetch();
+        require_once(BASE_PATH . '/template/admin/categories/edit.php');
+    }
+    public function update($request, $id)
+    {
+        $db = new Database();
+        $db->update("categories", $id, array_keys($request), $request);
+        $this->redirect("admin/category");
+    }
+    public function delete($id)
+    {
+        $db = new Database();
+        $db->delete("categories", $id);
+        $this->redirect("admin/category");
+    }
 }
