237c7072b1aff5e9bafab7ee823381eaef4c7f8e
behzad <behzad.kermanii@gmail.com>
2023-06-01 11:20:19 +0330

    dashboard done

--- activities/Admin/Dashboard.php
+++ activities/Admin/Dashboard.php
@@ -18,6 +18,9 @@ class Dashboard extends Admin
         $commentCount = $db->select("SELECT count(*) FROM comments")->fetch();
         $unseenCommentCount = $db->select("SELECT count(*) FROM comments where status = 'unseen'")->fetch();
         $approvedCommentCount = $db->select("SELECT count(*) FROM comments where status = 'approved'")->fetch();
+        $mostViewedPosts = $db->select("SELECT * from posts order by view desc limit 0,5")->fetchAll();
+        $mostCommentedPosts = $db->select("SELECT posts.id,posts.title,count(comments.post_id) AS comments_count from posts left join comments on posts.id = comments.post_id group by posts.id order by comments_count desc limit 0,5")->fetchAll();
+        $lastComments = $db->select("SELECT comments.id,comments.comment,comments.status,comments.post_id,users.username from comments,users where comments.user_id = users.id order by comments.created_at desc limit 0,5")->fetchAll();
 
         require_once(BASE_PATH . '/template/admin/dashboard/index.php');
     }
--- template/admin/dashboard/index.php
+++ template/admin/dashboard/index.php
@@ -10,7 +10,9 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
     <div class="col-sm-6 col-lg-3">
         <a href="<?= url("admin/category") ?>" class="text-decoration-none">
             <div class="card text-white bg-gradiant-green-blue mb-3">
-                <div class="card-header d-flex justify-content-between align-items-center"><span><i class="fas fa-clipboard-list"></i> Categories</span> <?= $categoryCount['count(*)'] ?> <span class="badge badge-pill right"></span></div>
+                <div class="card-header d-flex justify-content-between align-items-center"><span><i
+                            class="fas fa-clipboard-list"></i> Categories</span> <?= $categoryCount['count(*)'] ?> <span
+                        class="badge badge-pill right"></span></div>
                 <div class="card-body">
                     <section class="font-12 my-0"><i class="fas fa-clipboard-list"></i> GO TO Categories!</section>
                 </div>
@@ -20,11 +22,15 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
     <div class="col-sm-6 col-lg-3">
         <a href="<?= url("admin/user") ?>" class="text-decoration-none">
             <div class="card text-white bg-juicy-orange mb-3">
-                <div class="card-header d-flex justify-content-between align-items-center"><span><i class="fas fa-users"></i> Users</span> <?= $userCount['count(*)'] ?> <span class="badge badge-pill right"></span></div>
+                <div class="card-header d-flex justify-content-between align-items-center"><span><i
+                            class="fas fa-users"></i> Users</span> <?= $userCount['count(*)'] ?> <span
+                        class="badge badge-pill right"></span></div>
                 <div class="card-body">
                     <section class="d-flex justify-content-between align-items-center font-12">
-                        <span class=""><i class="fas fa-users-cog"></i> Admin <span class="badge badge-pill mx-1"></span><?= $adminCount['count(*)'] ?></span>
-                        <span class=""><i class="fas fa-user"></i> All Users <span class="badge badge-pill mx-1"></span><?= $userCount['count(*)'] + $adminCount['count(*)'] ?></span>
+                        <span class=""><i class="fas fa-users-cog"></i> Admin <span
+                                class="badge badge-pill mx-1"></span><?= $adminCount['count(*)'] ?></span>
+                        <span class=""><i class="fas fa-user"></i> All Users <span
+                                class="badge badge-pill mx-1"></span><?= $userCount['count(*)'] + $adminCount['count(*)'] ?></span>
                     </section>
                 </div>
             </div>
@@ -33,11 +39,14 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
     <div class="col-sm-6 col-lg-3">
         <a href="<?= url("admin/post") ?>" class="text-decoration-none">
             <div class="card text-white bg-dracula mb-3">
-                <div class="card-header d-flex justify-content-between align-items-center"><span><i class="fas fa-newspaper"></i> Article</span><?= $postCount['count(*)'] ?> <span class="badge badge-pill right"></span>
+                <div class="card-header d-flex justify-content-between align-items-center"><span><i
+                            class="fas fa-newspaper"></i> Article</span><?= $postCount['count(*)'] ?> <span
+                        class="badge badge-pill right"></span>
                 </div>
                 <div class="card-body">
                     <section class="d-flex justify-content-between align-items-center font-12">
-                        <span class=""><i class="fas fa-bolt"></i> Views <span class="badge badge-pill mx-1"></span><?= $postViews['SUM(view)'] ?></span>
+                        <span class=""><i class="fas fa-bolt"></i> Views <span
+                                class="badge badge-pill mx-1"></span><?= $postViews['SUM(view)'] ?></span>
                     </section>
                 </div>
             </div>
@@ -46,13 +55,17 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
     <div class="col-sm-6 col-lg-3">
         <a href="<?= url("admin/comment") ?>" class="text-decoration-none">
             <div class="card text-white bg-neon-life mb-3">
-                <div class="card-header d-flex justify-content-between align-items-center"><span><i class="fas fa-comments"></i> Comment</span> <span class="badge badge-pill right"><?= $commentCount['count(*)'] ?></span>
+                <div class="card-header d-flex justify-content-between align-items-center"><span><i
+                            class="fas fa-comments"></i> Comment</span> <span
+                        class="badge badge-pill right"><?= $commentCount['count(*)'] ?></span>
                 </div>
                 <div class="card-body">
                     <!--                        <h5 class="card-title">Info card title</h5>-->
                     <section class="d-flex justify-content-between align-items-center font-12">
-                        <span class=""><i class="far fa-eye-slash"></i> Unseen <span class="badge badge-pill mx-1"></span><?= $unseenCommentCount['count(*)'] ?></span>
-                        <span class=""><i class="far fa-check-circle"></i> Approved <span class="badge badge-pill mx-1"></span><?= $approvedCommentCount['count(*)'] ?></span>
+                        <span class=""><i class="far fa-eye-slash"></i> Unseen <span
+                                class="badge badge-pill mx-1"></span><?= $unseenCommentCount['count(*)'] ?></span>
+                        <span class=""><i class="far fa-check-circle"></i> Approved <span
+                                class="badge badge-pill mx-1"></span><?= $approvedCommentCount['count(*)'] ?></span>
                     </section>
                 </div>
             </div>
@@ -77,19 +90,20 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
                     </tr>
                 </thead>
                 <tbody>
-
+                    <?php foreach ($mostViewedPosts as $mostViewedPost) : ?>
                     <tr>
                         <td>
                             <a class="text-primary" href="">
-                                ss
+                                <?= $mostViewedPost['id'] ?>
                             </a>
                         </td>
                         <td>
-                            ss
+                            <?= $mostViewedPost['title'] ?>
                         </td>
-                        <td><span class="badge badge-secondary">ss</span></td>
+                        <td><span class="badge badge-secondary"> <?= $mostViewedPost['view'] ?>
+                            </span></td>
                     </tr>
-
+                    <?php endforeach; ?>
 
                 </tbody>
             </table>
@@ -110,19 +124,21 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
                     </tr>
                 </thead>
                 <tbody>
-
+                    <?php foreach ($mostCommentedPosts as $mostCommentedPost) : ?>
                     <tr>
                         <td>
                             <a class="text-primary" href="">
-                                ss
+                                <?= $mostCommentedPost['id'] ?>
                             </a>
                         </td>
                         <td>
-                            ss
-                        </td>
-                        <td><span class="badge badge-success">ss</span></td>
-                    </tr>
+                            <?= $mostCommentedPost['title'] ?>
 
+                        </td>
+                        <td><span class="badge badge-success"> <?= $mostCommentedPost['comments_count'] ?>
+                            </span></td>
+                    </tr>
+                    <?php endforeach; ?>
 
                 </tbody>
             </table>
@@ -144,23 +160,26 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
                 </thead>
                 <tbody>
 
-
+                    <?php foreach ($lastComments as $lastComment) : ?>
                     <tr>
                         <td>
                             <a class="text-primary" href="">
-                                ss
+                                <?= $lastComment['id'] ?>
+
                             </a>
                         </td>
                         <td>
-                            ss
+                            <?= $lastComment['username'] ?>
+
                         </td>
                         <td>
-                            ss
+                            <?= $lastComment['comment'] ?>
                         </td>
-                        <td><span class="badge badge-warning">ss</span></td>
+                        <td><span class="badge badge-warning"> <?= $lastComment['status'] ?>
+                            </span></td>
                     </tr>
 
-
+                    <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
